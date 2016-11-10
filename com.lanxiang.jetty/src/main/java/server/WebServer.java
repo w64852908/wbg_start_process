cacecb5b7472fea2ece88e7ce40a7d7c46f20ee2
lanjing <lanjing@haizhi.com>
2016-11-10 11:05:27 +0800

    add swagger part and jetty run environment

---
+++ com.lanxiang.jetty/src/main/java/server/WebServer.java
@@ -0,0 +1,56 @@
+package server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+import org.eclipse.jetty.webapp.WebXmlConfiguration;
+
+import java.io.File;
+import java.net.URL;
+
+/**
+ * Created by lanxiang on 2016/11/1.
+ */
+@Slf4j
+public class WebServer {
+
+    private static void start(int port, String webappPath) {
+        log.info("Start webapp: {}", webappPath);
+        String host = "localhost";
+        String path = "/";
+        String uri = "http://" + host + ":" + port + path;
+        try {
+            WebAppContext context = new WebAppContext();
+            context.setDisplayName("JettyContext");
+            context.setThrowUnavailableOnStartupException(true);
+            context.setContextPath(path);
+            context.setResourceBase(webappPath);
+            context.setWelcomeFiles(new String[]{"index.json"});
+            context.setConfigurations(new Configuration[]{
+                    new WebXmlConfiguration(),
+                    new WebInfConfiguration(),
+                    new ExtMetaInfConfiguration()
+            });
+            //TODO add eventlistener
+            //TODO add filter
+            Server server = new Server(port);
+            server.setHandler(context);
+            server.start();
+            log.info("Application started at {}", uri);
+
+        } catch (Exception e) {
+            log.error("Http server error." + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        URL url = Thread.currentThread().getContextClassLoader().getResource(".");
+        String classes = url.getFile();
+        File targetClassFolder = new File(classes);
+        String parentPath = targetClassFolder.getParentFile().getParent();
+        start(9090, parentPath + "/src/main/java/webapp");
+    }
+
+}
